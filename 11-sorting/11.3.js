2ce9def3fdde3d7b67bacee88f0755fac90f0ebd
Michelle Lee <michelle.mh.lee92@gmail.com>
2016-02-10 13:47:52 -0800

    (refactor) Spacing issues

--- 11-sorting/11.3.js
+++ 11-sorting/11.3.js
@@ -12,14 +12,13 @@ var findPivot = function(arr, low, high) {
   var mid = Math.floor((low + high) / 2);
   if (mid < high && arr[mid] > arr[mid + 1]) {
     return mid;
-  }
-  if (mid > low && arr[mid] < arr[mid - 1]) {
+  } else if (mid > low && arr[mid] < arr[mid - 1]) {
     return mid - 1;
-  }
-  if (arr[low] >= arr[mid]) {
+  } else if (arr[low] >= arr[mid]) {
     return findPivot(arr, low, mid - 1);
+  } else {
+    return findPivot(arr, mid + 1, high);
   }
-  return findPivot(arr, mid + 1, high);
 };
 
 var binarySearch = function(arr, target) {
